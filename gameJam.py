ebfecea32268ea8eb5b8b7aa3a3d659cfd5ca3e0
Sarah <sarah@cleancodecompany.com>
2023-10-14 17:54:39 +0100

    Start of first GameJam game :D

---
+++ gameJam.py
@@ -0,0 +1,17 @@
+
+# get user's name
+def nameFinder():
+    print("Hello there traveller....")
+    print(".....")
+    print("What is your name?")
+    username = input("Your Name:")
+    print("AH,... Hello  " + username +
+          " I haven't heard that name in very long time.... ")
+
+# set up athmosphere
+# first question / pet or pick up the black cat
+#
+
+
+# Call all functions
+nameFinder()
